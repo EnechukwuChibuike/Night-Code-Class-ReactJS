7871f9865243298953554a99aa7bc22c5792cb39
Enechukwu Chibuike <enechukwuchibuike0@gmail.com>
2024-03-01 03:17:54 +0100

    added async and await

--- src/components/FetchData.jsx
+++ src/components/FetchData.jsx
@@ -1,19 +1,34 @@
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 
 function FetchData() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchData = () => {
-      fetch("https://fakestoreapi.com/products")
-        .then((response) => response.json())
-        .then((result) => {
-          const data = result;
+  useLayoutEffect(() => {
+    // const fetchData = () => {
+    //   fetch("https://fakestoreapi.com/products")
+    //     .then((response) => response.json())
+    //     .then((result) => {
+    //       const data = result;
 
-          setData(data);
-        })
-        .catch((error) => console.log(error));
-      console.log(data);
+    //       setData(data);
+    //     })
+    //     .catch((error) => console.log(error));
+    //   console.log(data);
+    // };
+
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch("https://fakestoreapi.com/products");
+
+        const result = await response.json();
+
+        setData(result);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
     };
 
     fetchData();
@@ -21,6 +36,8 @@ function FetchData() {
 
   return (
     <section className="flex flex-wrap min-h-screen justify-around ">
+      {loading && <h1 className="text-5xl">Loading...</h1>}
+
       {data.map((product) => (
         <figure
           key={product.id}

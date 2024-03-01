15274fccfcb2ebcb86bae05436a5520901b02f79
Enechukwu Chibuike <enechukwuchibuike0@gmail.com>
2024-03-01 03:20:19 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
 import "./App.css";
-import Counter from "./components/Counter";
-import FetchData from "./components/FetchData";
-import StickyHeader from "./components/StickyHeader";
 
 function App() {
-  return (
-    <main>
-      <StickyHeader />
-      <Counter />
-      <FetchData />
-    </main>
-  );
+  return <main></main>;
 }
 
 export default App;

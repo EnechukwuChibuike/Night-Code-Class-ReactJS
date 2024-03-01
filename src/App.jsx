83dd0bb0588746c60c283a6d5bc139f2cad3e956
Enechukwu Chibuike <enechukwuchibuike0@gmail.com>
2024-03-01 03:33:37 +0100

    added react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
-  return <main></main>;
+  return (
+    <main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
